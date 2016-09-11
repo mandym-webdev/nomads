f24ecec091316746c15d58a62eb576bac9a60007
Mandy Moore <mandy@mandym.com>
2016-09-11 12:23:13 +0200

    updated form params

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -20,38 +20,8 @@ class SubmissionsController < ApplicationController
   private
 
   def submission_params
-    params.require(:submission).permit(:submission_link)
+    params.require(:submission).permit(:submission_link, :title, :location, :bedrooms, :bathrooms, :img1, :img2, :img3)
   end
 end
 
 
-
-# def create
-#     @question = Question.find(params[:question_id])
-#     if @question.answers.create( answer_params )
-#       redirect_to @question
-#     else
-#       render 'new'
-#     end
-#   end
-
-
-#    def create
-#     @suggestion = Suggestion.new(suggestion_params)
-#     if user_signed_in?
-#       @suggestion.user = current_user
-#       if @suggestion.save
-#         flash[:success] = object_flash_message_for(@suggestion)
-#         redirect_to after_update_object_path_for(@suggestion)
-#       else
-#         render :new
-#       end
-#     else
-#       if @suggestion.valid?
-#         cache_pending_object(@suggestion)
-#         redirect_to preview_path(class_name: 'suggestion')
-#       else
-#         render :new
-#       end
-#     end
-#   end
