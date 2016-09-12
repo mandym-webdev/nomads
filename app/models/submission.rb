9c5d7d7d9c16fe9659307ec44a9b435bc1b55915
Mandy Moore <mandy@mandym.com>
2016-09-12 11:04:45 +0200

    installed devise

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ActiveRecord::Base
 
-  validates :submission_link, presence: true { message: "Please enter a listing URL." }, uniqueness: true { message: "This listing seems to already exist" }
+  validates :submission_link, presence: true, uniqueness: true 
 
 end
 
